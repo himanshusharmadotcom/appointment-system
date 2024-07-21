5a0fb03b33a7361425654815587e8cc98f98b91d
Himanshu <hs7772043@gmail.com>
2024-07-21 12:42:59 +0530

    installed react-router-dom and created the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
 
 const App = () => {
   return (
-    <div className='bg-red-700'>This is app</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

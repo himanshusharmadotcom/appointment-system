23dfb441795fecf9db8b83c706f332dca4d94c02
Himanshu <hs7772043@gmail.com>
2024-07-22 08:35:40 +0530

    added files

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ import express from 'express'
 import connectDB from './config/db.js';
 import cors from 'cors'
 import dotenv from 'dotenv'
-
 import userRoute from './routes/userRoutes.js'
 import authRoutes from './routes/authRoutes.js'
 import appointmentRoutes from './routes/appointmentRoutes.js'
+import path from 'path';
 
 dotenv.config();
 
@@ -14,14 +14,22 @@ const app = express();
 // Connect Database
 connectDB();
 
+const __dirname = path.resolve(); // captures the absolute path of the directory where this code is being executed
+
 // Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false })); //allows the client to send and reciece data in json format
 app.use(cors());
 
 // Define Routes
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 const PORT = process.env.PORT || 3000;
 
